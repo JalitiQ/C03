befd4d8f5b726d947a888b35f018cb8be58abcf9
JalitiQ <contact@jullienl.com>
2025-07-01 15:55:02 +0200

    ex02 finish

--- ex02/ft_strcat.c
+++ ex02/ft_strcat.c
@@ -1,25 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lilian <lilian@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/01 12:09:39 by lilian            #+#    #+#             */
-/*   Updated: 2025/07/01 15:28:28 by lilian           ###   ########.fr       */
+/*   Created: 2025/07/01 15:34:47 by lilian            #+#    #+#             */
+/*   Updated: 2025/07/01 15:54:26 by lilian           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-
-int	ft_strcmp(char *s1, char *s2)
+char	*ft_strcat(char *dest, char *src)
 {
 	int	i;
+	int	j;
 
 	i = 0;
-	while (s1[i] && s2[i] == s1[i])
+	j = 0;
+	while (dest[i])
 	{
 		i++;
 	}
-	return (s1[i] - s2[i]);
+	while (src[j])
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
+
+// int main(void)
+//{
+//	char s1[] = "dijjeicd";
+//	char s2[] = "dijjeicd";
+//	printf("%s", ft_strcat(s1, s2));
+//}
