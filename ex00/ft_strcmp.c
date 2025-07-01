3d398b930b43333c58b966a365daf1670064a8f0
JalitiQ <contact@jullienl.com>
2025-07-01 13:50:30 +0200

    gitignore

--- ex00/ft_strcmp.c
+++ ex00/ft_strcmp.c
@@ -6,7 +6,27 @@
 /*   By: lilian <lilian@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/01 12:09:39 by lilian            #+#    #+#             */
-/*   Updated: 2025/07/01 12:10:05 by lilian           ###   ########.fr       */
+/*   Updated: 2025/07/01 13:50:30 by lilian           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i] == s1[i])
+	{
+		i++;
+	}
+	return s1[i] - s2[i];
+}
+
+int main(void)
+{
+	char s1t[] = "piscine";
+	char s2t[] = "pascine";
+	printf("%d", ft_strcmp(s1t, s2t));
+}
